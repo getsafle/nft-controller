abcac57c854476f3722b1db578ceaef0c3f5d6f4
Shubham Singhal <aggshubh95@gmail.com>
2021-04-21 14:35:35 +0530

    Added method to get details of particular nft in a wallet

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+const HELPER = require('./utils/helper');
+const { OPENSEA_API_KEY } = require('./config/index');
+
+class NftController {
+    async getNftDetails(publicAddress, contractAddress) {
+        const url = await HELPER.getUserNftDataApi({ publicAddress, contractAddress });
+        const apiKey = OPENSEA_API_KEY;
+        const { response, error } = await HELPER.getRequest({ url, apiKey });
+        if (error) {
+            const { status, statusText } = error;
+            return { error:{ status, statusText } };
+        }
+
+        const { assets } = response;
+        const nftDetails = JSON.parse(JSON.stringify(assets[0]));
+        return { response: nftDetails };
+    }
+    
+}
+
+module.exports = { NftController: NftController }
